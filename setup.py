7524e138705b830cc687e54eb340fd703a54b945
Nicolas Galtier <nicolas.galtier@setec.com>
2020-01-14 15:40:20 -0500

    0.0.3

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name="SiNAPS",
-    version="0.0.2",
+    version="0.0.3",
     author="Claire Guerrier",
     author_email="claire.guerrier@univ-cotedazur.fr",
     description="Neuronal simulation package",
