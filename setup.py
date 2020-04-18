85e4743c43ff91f8eb228bc398bf0c26d2bcae54
Claire Guerrier <claire.guerrier@gmail.com>
2020-04-18 11:38:11 +0200

    v0.0.4

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name="SiNAPS",
-    version="0.0.3",
+    version="0.0.4",
     author="Claire Guerrier",
     author_email="claire.guerrier@univ-cotedazur.fr",
     description="Neuronal simulation package",
@@ -13,8 +13,9 @@ setuptools.setup(
         "pandas",
         "quantiphy",
         "scipy>=1.0.0",
-        "matplotlib",
         "hvplot",
+        "networkx"
+        "datashader"
         "tqdm",
         "numba"
     ],
