88253354718eca5316943cdbcdf6a1296e8ef3af
Claire Guerrier <claire.guerrier@gmail.com>
2021-10-02 20:13:39 -0400

    Modif

--- setup.py
+++ setup.py
@@ -6,7 +6,8 @@ setuptools.setup(
     author="Claire Guerrier",
     author_email="claire.guerrier@univ-cotedazur.fr",
     description="Neuronal simulation package",
-    packages=setuptools.find_packages(),
+    packages=setuptools.find_packages('src'),
+    package_dir={'': 'src'},
     python_requires='>=3.6',
     install_requires=[
         "numpy",
@@ -14,8 +15,8 @@ setuptools.setup(
         "quantiphy",
         "scipy>=1.0.0",
         "hvplot",
-        "networkx"
-        "datashader"
+        "networkx",
+        "datashader",
         "tqdm",
         "numba"
     ],
