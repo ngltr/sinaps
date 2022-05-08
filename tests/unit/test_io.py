1a34ed3e0309afc8ab102d65973616d7c8709535
Nicolas <49352943+ngltr@users.noreply.github.com>
2022-05-07 23:27:10 -0400

    Update test_io.py

--- tests/unit/test_io.py
+++ tests/unit/test_io.py
@@ -1,11 +1,10 @@
 # -*- coding: utf-8 -*-
 from sinaps.data import io
-import ssl
+import pytest
 
 
-
+@pytest.mark.skip #skipping this test because ssl issue with neuromorpho website
 def test_read_swc():
-    ssl._create_default_https_context = ssl._create_unverified_context #fix url open error 
     filename = "http://neuromorpho.org/dableFiles/chang/CNG%20version/V247fs-MT-Untreated-56.CNG.swc"
     nrn = io.read_swc(filename)
     assert nrn.nb_nodes == 128
