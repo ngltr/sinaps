a3467f984c55c1aa35b1e93da06492b285290ae7
Nicolas <49352943+ngltr@users.noreply.github.com>
2022-05-11 21:49:58 -0400

    version 0.3.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name="SiNAPS",
-    version="0.3.1",
+    version="0.3.2",
     author="Claire Guerrier",
     author_email="claire.guerrier@univ-cotedazur.fr",
     description="Neuronal simulation package",
