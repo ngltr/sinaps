13df47c3755832f8893cb7368e3c601292113192
Fricker95 <nicolas.fricker@gmail.com>
2024-01-22 16:28:22 +0100

    remove pygraphviz from setup

--- setup.py
+++ setup.py
@@ -20,6 +20,6 @@ setuptools.setup(
         "datashader",
         "tqdm",
         "numba",
-        "pygraphviz" # for macOS use brew install graphviz and build package using proper headers
+        #"pygraphviz" # for macOS use brew install graphviz and build package using proper headers
     ],
 )
